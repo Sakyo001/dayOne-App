a35e3edbbc70c34f19609b74bc18b1dfe1fe61b7
unknown <masagnayjohnroel11@gmail.com>
2025-01-10 22:29:23 +0800

    day 1

---
+++ app/constants.tsx
@@ -0,0 +1,33 @@
+// constants.js
+export const weatherData = [
+    {
+        day: "Monday",
+        temperature: "25°C",
+        condition: "Sunny",
+        icon: "☀️",
+    },
+    {
+        day: "Tuesday",
+        temperature: "22°C",
+        condition: "Cloudy",
+        icon: "☁️",
+    },
+    {
+        day: "Wednesday",
+        temperature: "18°C",
+        condition: "Rainy",
+        icon: "🌧️",
+    },
+    {
+        day: "Thursday",
+        temperature: "28°C",
+        condition: "Sunny",
+        icon: "☀️",
+    },
+    {
+        day: "Friday",
+        temperature: "24°C",
+        condition: "Windy",
+        icon: "🌬️",
+    },
+];
